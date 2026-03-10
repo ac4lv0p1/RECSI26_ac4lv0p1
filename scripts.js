018b4aa1de01760664ccf6d077f84f925c618396
ac4lv0p1 <acalvopic@unex.es>
2026-03-10 12:15:15 +0100

    project update

--- scripts.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const hora = document.createElement("p");
-    hora.textContent = `Hora: ${new Date().toLocaleTimeString()}`;
-    document.body.prepend(hora);
-});
